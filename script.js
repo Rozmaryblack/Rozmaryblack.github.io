2f7cabd27bf89054807d79f12fa61789b01145b1
Rozmaryblack <56887794+Rozmaryblack@users.noreply.github.com>
2019-10-23 22:08:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function wrt(x)
+				{
+				document.getElementById("razv").innerHTML=x;
+				if (x!="") {document.getElementById("sver").innerHTML="Скрыть";}
+				else {document.getElementById("sver").innerHTML=""; document.getElementById("razv").innerHTML="Нажмите. Здесь немного информации обо мне с:";}
+				}
